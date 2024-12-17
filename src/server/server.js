6fbcceb2157c891525db01e2347af0fe36493349
Hisyan Qiyad Al-Fityan <hisyanqaf@gmail.com>
2024-12-17 13:04:18 +0700

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,30 +1,28 @@
 require('dotenv').config();
-
-
 const Hapi = require('@hapi/hapi');
 const routes = require('../server/routes');
 const loadModel = require('../services/loadModel');
 const InputError = require('../exceptions/InputError');
-
-
-
+ 
 (async () => {
     const server = Hapi.server({
-        port: 3000,
+        port: process.env.PORT || 3000,
         host: '0.0.0.0',
         routes: {
             cors: {
               origin: ['*'],
             },
         },
-    })
-
+    });
+ 
     const model = await loadModel();
     server.app.model = model;
-
-    server.route(routes);  // Akan dibahas lebih lanjut setelah pembahasan extension.
+ 
+    server.route(routes);
+ 
     server.ext('onPreResponse', function (request, h) {
         const response = request.response;
+ 
         if (response instanceof InputError) {
             const newResponse = h.response({
                 status: 'fail',
@@ -33,14 +31,16 @@ const InputError = require('../exceptions/InputError');
             newResponse.code(response.statusCode)
             return newResponse;
         }
+ 
         if (response.isBoom) {
             const newResponse = h.response({
                 status: 'fail',
                 message: response.message
-            });
-            newResponse.code(response.output.statusCode);
+            })
+            newResponse.code(response.output.statusCode)
             return newResponse;
         }
+ 
         return h.continue;
     });
  
