ab38ffe170c977b07644352da2b00f484672d21c
Hisyan Qiyad Al-Fityan <hisyanqaf@gmail.com>
2024-12-17 13:06:25 +0700

    Update storeData.js

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -1,21 +1,10 @@
 const { Firestore } = require('@google-cloud/firestore');
-const path = require('path');
-
-const pathKey = path.resolve('./submissionmlgc-hisyan-a9e2a77800e8')
-     
-async function storeData(id, data) {
-
-  try {
-    const db = new Firestore({
-      projectId: 'submissionmlgc-hisyan',
-      keyFilename: pathKey,
-    });
  
-    const predictCollection = db.collection('prediction');
-    return predictCollection.doc(id).set(data);
-  }catch(error) {
-    console.error(error);
-  }
+async function storeData(id, data) {
+  const db = new Firestore();
+ 
+  const predictCollection = db.collection('predictions');
+  return predictCollection.doc(id).set(data);
 }
  
 module.exports = storeData;
